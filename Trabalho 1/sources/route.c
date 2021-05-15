a4b2e0a9a0e8ff8235967ac3c654fdc60bc9e469
VictorHenrique <victor.hss@hotmail.com>
2021-05-14 22:52:57 -0300

    Fixed header reading bug

--- Trabalho 1/sources/route.c
+++ Trabalho 1/sources/route.c
@@ -32,6 +32,21 @@ struct _ROUTE{
     char *color;
 };
 
+/* 
+    Stores header string fields with its respective value (@word), ignoring '\0' and filling with '@'
+    If needed, this function can return @word length
+    @size is the field size in bytes
+*/
+void header_strings_creation(char *header_field, char *word, int size){
+    int cur_length = 0;
+    for (; word[cur_length] != '\0'; cur_length++) 
+        header_field[cur_length] = word[cur_length];
+
+    size -= 1;
+    for (; size > cur_length; size--)
+        header_field[size] = '@';
+}
+
 void create_header(ROUTE_HEADER header, WORDS *header_list){
     header.status = '0';
     header.next_reg = 175;
@@ -40,10 +55,10 @@ void create_header(ROUTE_HEADER header, WORDS *header_list){
 
     char **words = get_word_list(header_list);
     if (words != NULL){
-        memcpy(&(header.code_description[15]), words[0], ((strlen(words[0]) - 1) * sizeof(char)));
-        memcpy(&(header.card_description[13]), words[1], ((strlen(words[1]) - 1) * sizeof(char)));
-        memcpy(&(header.name_description[13]), words[2], ((strlen(words[2]) - 1) * sizeof(char)));
-        memcpy(&(header.color_description[24]), words[3], ((strlen(words[3]) - 1) * sizeof(char)));
+        header_strings_creation(header.code_description, words[0], 15);
+        header_strings_creation(header.card_description, words[1], 13);
+        header_strings_creation(header.name_description, words[2], 13);
+        header_strings_creation(header.color_description, words[3], 24);
     }
 }
 
@@ -58,7 +73,6 @@ void create_route_binary(FILE *csv_fp, FILE *bin_fp){
     printf("Header: %s\n", header_line);
     print_word_list(header_list);
 
-    // ESSA POHA TÁ ZUADO V
     ROUTE_HEADER header;
     create_header(header, header_list);
 
