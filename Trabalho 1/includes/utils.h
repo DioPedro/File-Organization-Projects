cc939412ef721886888efb7fa792e12361ab956e
DioPedro <diogenes.pedro06@gmail.com>
2021-05-17 08:39:10 -0300

    Same as last commit, this one just didn't commited? kkkk

--- Trabalho 1/includes/utils.h
+++ Trabalho 1/includes/utils.h
@@ -6,7 +6,7 @@
 #include <string.h>
 #include "str.h"
 
-void strings_creation(char *header_field, char *word, int size);
+void strings_creation(char *reg_field, char *word, int size);
 void write_data_strings(FILE *bin_fp, char *data_field, int size);
 void free_data(WORDS *word_list, char *reg_line);
 
